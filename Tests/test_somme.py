96ad3c11c2941d0987c86f9da2d4372bded0c585
Ismatou <ismatousadio.souare@edu.univ-paris13.fr>
2023-01-26 21:42:37 +0100

    tester une fonction avec pytest  avec mark pour qu'il produise un warring et non bloqué le test

--- Tests/test_somme.py
+++ Tests/test_somme.py
@@ -1,4 +1,5 @@
 from SOMME.add import addition
+import pytest
 
 def test_somme():
     # arrange
@@ -10,7 +11,12 @@ def test_somme():
     assert res == 7
     
 def test_somme_5_2_is_less_than_8():
-    assert addition(5, 2) < 8
+    assert addition(4, 3) < 8
 
 def test_somme_5_2_is_more_than_6():
-    assert addition(5, 2) > 6
+    assert addition(6, 1) > 6
+    
+@pytest.mark.xfail  
+def test_somme_expect_to_fail():
+    assert addition(2,2) == 5
+    
